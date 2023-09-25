e059fa4017be8a8419fe6e3b7bf7e5280d4dea84
Minhduc027 <minhduylongthuan@gmail>
2023-09-25 20:42:12 +0700

    map user

--- src/main/java/com/fpt/MeetLecturer/Mapper/mapUser.java
+++ src/main/java/com/fpt/MeetLecturer/Mapper/mapUser.java
@@ -1,4 +1,16 @@
 package com.fpt.MeetLecturer.Mapper;
 
+import com.fpt.MeetLecturer.BusinessModel.UserDTO;
+import com.fpt.MeetLecturer.EntityModel.User;
+import org.modelmapper.ModelMapper;
+
 public class mapUser {
+    public static final ModelMapper modelMapper = new ModelMapper();
+    public UserDTO mapUserToUserDTO(User user){
+        return modelMapper.map(user, UserDTO.class);
+    }
+    public User mapUserDTOToUser(UserDTO userDTO){
+        return modelMapper.map(userDTO, User.class);
+    }
+
 }
