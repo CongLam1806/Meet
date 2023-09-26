32dbc79c88f84e12a045099e176512a6afdb5314
TranCongLam <lamtc1806@gmail.com>
2023-09-26 12:37:46 +0700

    update

--- src/main/java/com/fpt/MeetLecturer/mapper/mapUser.java
+++ src/main/java/com/fpt/MeetLecturer/mapper/mapUser.java
@@ -3,14 +3,31 @@ package com.fpt.MeetLecturer.mapper;
 import com.fpt.MeetLecturer.business.UserDTO;
 import com.fpt.MeetLecturer.entity.User;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
-public class mapUser {
-    public static final ModelMapper modelMapper = new ModelMapper();
-    public UserDTO mapUserToUserDTO(User user){
-        return modelMapper.map(user, UserDTO.class);
-    }
-    public User mapUserDTOToUser(UserDTO userDTO){
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class MapUser {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static User convertUserDTOToUser(UserDTO userDTO){
         return modelMapper.map(userDTO, User.class);
     }
 
+    public static List<User> convertListToUser(List<UserDTO> usersDTO){
+        List<User> list = new ArrayList<>();
+        usersDTO.forEach(userDTO -> list.add(convertUserDTOToUser(userDTO)));
+        return list;
+    }
+
+    public static UserDTO convertUserToUserDTO(User user){
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public static List<UserDTO> convertListToUserDTO(List<User> users){
+        List<UserDTO> list = new ArrayList<>();
+        users.forEach(user -> list.add(convertUserToUserDTO(user)));
+        return list;
+    }
 }
