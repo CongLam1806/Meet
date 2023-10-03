9aeebe501c7af4ec35813e3620d8bf8622e2738f
Minhduc027 <minhduylongthuan@gmail>
2023-10-03 23:03:17 +0700

    minor change

--- src/main/java/com/fpt/MeetLecturer/business/LocationDTO.java
+++ src/main/java/com/fpt/MeetLecturer/business/LocationDTO.java
@@ -12,15 +12,15 @@ import lombok.*;
 @AllArgsConstructor
 public class LocationDTO {
     private  int id;
+
     @NotBlank(message = "Address label cannot be blank!")
     @Size(min=3, message = "Address label should be at least 3 characters")
     @Size(max=30, message = "Address label should not be greater than 30 characters")
-
     private String name;
+
     @NotBlank(message = "Address cannot be blank!")
     @Size(min=5, message = "Address should be at least 5 characters")
     @Size(max=100, message = "Address should not be greater than 100 characters")
-
     private String address;
 
     private boolean status;
