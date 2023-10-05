6b7c3d64e4f6ba1021184d13a1153ac6cce995ba
truongthanhvu <truongthanhvu2337@gmail.com>
2023-10-05 20:04:38 +0700

    delete SwaggerConfig.java

--- src/main/java/com/fpt/MeetLecturer/MeetLecturerApplication.java
+++ src/main/java/com/fpt/MeetLecturer/MeetLecturerApplication.java
@@ -1,9 +1,16 @@
 package com.fpt.MeetLecturer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/myapp/", description = "Default Server URL")
+		}
+)
 public class MeetLecturerApplication {
 
 	public static void main(String[] args) {
