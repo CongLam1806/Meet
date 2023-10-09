f1eb9bba702aceecb09be84fb37d044f0496eb0a
Minhduc027 <minhduylongthuan@gmail>
2023-10-09 13:23:33 +0700

    Add new package Util, modify createUser function

---
+++ src/main/java/com/fpt/MeetLecturer/util/Utility.java
@@ -0,0 +1,30 @@
+package com.fpt.MeetLecturer.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utility {
+    final static String pattern = "\\b\\p{Alpha}{2}\\d{6}\\b"; // Matches 2 letters followed by 6 digits
+    public  boolean isStudent(String Email){
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(Email);
+        return matcher.find();
+
+    }
+    public  String extractStudentId(String Email){
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(Email);
+        if(matcher.find()){
+            return matcher.group().toUpperCase();
+        }
+        return null;
+    }
+//    public static void main(String[] args){
+//        String demo = "se123456@gmail.com";
+//        boolean check = isStudent(demo);
+//        System.out.println("check regex: " + check);
+//        String Id = extractStudentId(demo);
+//        System.out.println("Id extract: " + Id);
+//    }
+}
+
