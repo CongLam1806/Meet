e83bf6c635e178a7cc784ef52601077caa52b508
Minhduc027 <minhduylongthuan@gmail>
2023-10-12 10:33:02 +0700

    Update Name: User -> Account

--- src/main/java/com/fpt/MeetLecturer/entity/Lecturer.java
+++ src/main/java/com/fpt/MeetLecturer/entity/Lecturer.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Entity
 public class Lecturer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     //@Column(unique = true)
     private String Id;
 
