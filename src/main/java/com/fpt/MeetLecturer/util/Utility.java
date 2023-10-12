28bd2b329a259c9bda8d58e54e040e385a86ba59
Minhduc027 <minhduylongthuan@gmail>
2023-10-12 12:06:37 +0700

    fixed bug: cannot auto add role

--- src/main/java/com/fpt/MeetLecturer/util/Utility.java
+++ src/main/java/com/fpt/MeetLecturer/util/Utility.java
@@ -6,14 +6,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 @Component
 public class Utility {
-    final static String pattern = "\\b\\p{Alpha}{2}\\d{6}\\b"; // Matches 2 letters followed by 6 digits
-    public  boolean isStudent(String Email){
+    final static String pattern = "[A-Z,a-z]{2}[\\d]{6}"; // Matches 2 letters followed by 6 digits
+    public boolean isStudent(String Email){
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher = regex.matcher(Email);
         return matcher.find();
 
     }
-    public  String extractStudentId(String Email){
+    public static String extractStudentId(String Email){
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher = regex.matcher(Email);
         if(matcher.find()){
@@ -22,7 +22,7 @@ public class Utility {
         return null;
     }
 //    public static void main(String[] args){
-//        String demo = "se123456@gmail.com";
+//        String demo = "mimndse173605@fpt.edu.vn";
 //        boolean check = isStudent(demo);
 //        System.out.println("check regex: " + check);
 //        String Id = extractStudentId(demo);
