a9c7c3e7ad10dec1d8969dc4e1aa338815a3a2a6
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2023-10-16 00:43:05 +0700

    add composite key between many to many relationship

--- src/main/java/com/fpt/MeetLecturer/controller/SubjectController.java
+++ src/main/java/com/fpt/MeetLecturer/controller/SubjectController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/subject")
+@RequestMapping(path = "/api/subject")
 public class SubjectController {
 
     @Autowired
