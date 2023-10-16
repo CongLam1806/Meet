0cb2816c87188f2799c7660e9268a1f23fe179e2
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2023-10-16 18:24:59 +0700

    minor change

--- src/main/java/com/fpt/MeetLecturer/config/CorsConfig.java
+++ src/main/java/com/fpt/MeetLecturer/config/CorsConfig.java
@@ -5,17 +5,33 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+////                .allowedOriginPatterns("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers")
+//                .maxAge(1440000);
+//    }
+//}
+
+
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-//                .allowedOriginPatterns("*")
+                .allowedOrigins("*","https://meetinglecturers.giakhang3005.com")
                 .allowedMethods("*")
-                .allowedHeaders("*");
-//                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers")
-//                .maxAge(1440000);
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers")
+                .maxAge(3600);
     }
 }
+
