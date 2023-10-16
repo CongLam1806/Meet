4e3460fc1a6c160745d280e19f6a57dfa2eefa0b
Minhduc027 <minhduylongthuan@gmail>
2023-10-16 12:52:20 +0700

    update location

--- src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
+++ src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
@@ -34,7 +34,7 @@ public class LocationController {
        return ResponseEntity.ok().body(locationService.createLocation(locationDTO));
     }
     //@CrossOrigin(origins = {"http://localhost:3000", "https://meet-production-52c7.up.railway.app/swagger-ui/index.html","https://meetinglecturers.giakhang3005.com"})
-    @PutMapping("/put")
+    @PutMapping("/mod")
     public  ResponseEntity<ResponseDTO> updateLocation(@Valid @RequestBody LocationDTO locationDTO, @RequestParam(value = "id") int id){
         return ResponseEntity.ok().body(locationService.updateLocation(locationDTO));
     }
