5934fff69f2aa5814f7acab25cf5f6470a8fc692
Minhduc027 <minhduylongthuan@gmail>
2023-10-16 16:18:06 +0700

    Cors update

--- src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
+++ src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(path="/api/location")
 public class LocationController {
     @Autowired
