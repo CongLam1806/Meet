d69044d3a862eec33f7001fade4861175adca019
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2023-10-16 11:35:54 +0700

    Add Major Crud

--- src/main/java/com/fpt/MeetLecturer/controller/MajorController.java
+++ src/main/java/com/fpt/MeetLecturer/controller/MajorController.java
@@ -1,11 +1,11 @@
 package com.fpt.MeetLecturer.controller;
 
 import com.fpt.MeetLecturer.business.MajorDTO;
+import com.fpt.MeetLecturer.business.ResponseDTO;
 import com.fpt.MeetLecturer.service.MajorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +20,22 @@ public class MajorController {
     public List<MajorDTO> getAllMajor(){
         return majorService.getAllMajor();
     }
+
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDTO> CreateNewMajor(@RequestBody MajorDTO dto){
+        return majorService.createMajor(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDTO> UpdateMajor(@RequestBody MajorDTO dto
+                                                                ,@PathVariable int id){
+        return majorService.updateMajor(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDTO> DeleteMajor(@PathVariable int id){
+        return  majorService.deleteMajor(id);
+    }
+
 }
