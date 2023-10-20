71b221ce54899a83dfec9978a7eea7bb0e6fa182
Minhduc027 <minhduylongthuan@gmail>
2023-10-20 22:25:14 +0700

    Update API path

--- src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
+++ src/main/java/com/fpt/MeetLecturer/controller/LocationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping(path="/api/location")
+@RequestMapping(path="/api/v1/location")
 public class LocationController {
     @Autowired
     private LocationService locationService;
