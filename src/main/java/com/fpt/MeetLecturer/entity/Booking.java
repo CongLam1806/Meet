2126f157091cd946b87020e84d059d08f6d68cd0
TranCongLam <lamtc1806@gmail.com>
2023-11-17 00:58:36 +0700

    update

--- src/main/java/com/fpt/MeetLecturer/entity/Booking.java
+++ src/main/java/com/fpt/MeetLecturer/entity/Booking.java
@@ -48,4 +48,11 @@ public class Booking {
         this.subject = subject;
         this.status = 2;
     }
+
+    public Booking(Slot slot, Student student, int status) {
+        this.slot = slot;
+        this.student = student;
+        this.subject = subject;
+        this.status = 2;
+    }
 }
