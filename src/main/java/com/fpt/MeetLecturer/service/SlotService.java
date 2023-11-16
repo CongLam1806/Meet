08b5b7447b470cf2254f18835d518cd8527c8b01
Minhduc027 <minhduylongthuan@gmail>
2023-11-16 15:07:57 +0700

    update check valid time,

--- src/main/java/com/fpt/MeetLecturer/service/SlotService.java
+++ src/main/java/com/fpt/MeetLecturer/service/SlotService.java
@@ -17,7 +17,10 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SlotService {
